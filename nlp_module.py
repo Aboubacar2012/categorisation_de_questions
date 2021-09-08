e69198f51caad5d0204290bbd68a8b605ebf9181
Cécile Guillot <64648386+Sylvariane@users.noreply.github.com>
2021-09-08 22:08:04 +0200

    add separators between parts

--- nlp_module.py
+++ nlp_module.py
@@ -7,6 +7,7 @@ import re
 from bs4 import BeautifulSoup
 from contractions import CONTRACTION_MAP
 import unicodedata
+from gensim.models import LdaMulticore, CoherenceModel
 
 """Functions are found in this article 
 https://towardsdatascience.com/a-practitioners-guide-to-natural-language-processing-part-i-processing-understanding-text-9f4abfd13e72"""
