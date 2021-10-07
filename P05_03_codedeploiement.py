96ee4c7e00afa78841a00297ea90e678b60cb429
Cécile Guillot <64648386+Sylvariane@users.noreply.github.com>
2021-10-07 13:54:12 +0200

    adding response link

--- P05_03_codedeploiement.py
+++ P05_03_codedeploiement.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, requests
 from werkzeug.utils import secure_filename
 import joblib
 import traceback
@@ -14,9 +14,9 @@ def index():
 
 @app.route("/predict", methods= ['GET','POST'])
 def predict():
-    title = request.form.get('title')
+    title = requests.form.get('title')
     print(title)
-    body = request.form.get('body')
+    body = requests.form.get('body')
     print(body)
     print("String format required for Machine Learning prediction")
     post = title + " " + body
@@ -26,6 +26,7 @@ def predict():
     keyword = transformer.inverse_transform(keyword)
     keyword = [x for x in keyword if x != ()]
     keyword = list(set(keyword))
+    response = requests.get(f'tag-generator-stackoverflow.herokuapp.com/')
     return render_template('index.html', prediction_text="Keywords suggested: {}".format(keyword))
 
 if __name__ == "__main__":
