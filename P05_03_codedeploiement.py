221b6ee5fd847100decc162cbc9921972ab9aed6
Cécile Guillot <64648386+Sylvariane@users.noreply.github.com>
2021-10-07 16:45:52 +0200

    correction organisation

--- P05_03_codedeploiement.py
+++ P05_03_codedeploiement.py
@@ -4,6 +4,9 @@ import joblib
 
 app = Flask(__name__, template_folder='templates', static_folder='templates/static')
 
+keyword_model = joblib.load("model_pipeline.pkl")
+transformer = joblib.load("mlb_transformer.pkl")
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -26,6 +29,4 @@ def predict():
                             prediction_text="Keywords suggested: {}".format(keyword))
 
 if __name__ == '__main__':
-    keyword_model = joblib.load("model_pipeline.pkl")
-    transformer = joblib.load("mlb_transformer.pkl")
     app.run(debug=True)
