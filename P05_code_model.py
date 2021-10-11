a97bc09f0f33a357cc493f5a7f9976dc04a23adb
Cécile Guillot <64648386+Sylvariane@users.noreply.github.com>
2021-10-11 11:52:11 +0200

    update model

--- P05_code_model.py
+++ P05_code_model.py
@@ -22,7 +22,7 @@ print("Loading dataset")
 t0 = time()
 path = "datasets/posts_clean.csv"
 data = pd.read_csv(path, encoding="utf-8")
-data = data.sample(frac=0.25,
+data = data.sample(frac=0.10,
                      random_state=42)
 print(data.head(3))
 print("Dataset Loaded")
